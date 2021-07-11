289adf12775ebfecb564431c16f429745796734f
SM SOHEL BISWAS <82491868+iamsohel98@users.noreply.github.com>
2021-07-11 12:09:24 +0530

    Update Stack_Normal .py

--- Stack_Normal .py
+++ Stack_Normal .py
@@ -12,3 +12,20 @@ stack.pop()
 print(stack)
 stack.pop()
 print(stack)
+
+# In LIFOQueue is LAST IN FIRST OUT. an iteam is queue is adding 
+# using PUT(iteam) method and To remove an element get(iteam) method.
+import queue
+stack = queue.LifoQueue(4)
+stack.put(50)
+stack.put(40)
+stack.put(10)
+stack.put(39)
+#stack.put(80)
+#print("Top of the Stack:",stack[-1])
+a = stack.get()
+print(a)
+b = stack.get()
+print(b)
+c = stack.get()
+print(c)
